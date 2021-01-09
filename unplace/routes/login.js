073ed5a227537436bd6ce0f0bb61b0718d3f3d73
leesing9 <63177746+leesing9@users.noreply.github.com>
2021-01-10 02:15:55 +0900

    v2.2

--- unplace/routes/login.js
+++ unplace/routes/login.js
@@ -5,14 +5,6 @@ const  fs = require('fs');
 const  router = express.Router();
 var    loglevel = 1;
 
-const   db = mysql.createConnection({
-      host: '49.50.165.106',        // DB서버 IP주소
-      port: 65007,               // DB서버 Port주소
-      user: 'root',            // DB접속 아이디
-      password: 'lees9814!@',  // DB암호
-      database: 'unplace'         //사용할 DB명
-  });
-  
 const  GetMainUI = (req, res) => {   // 메인화면을 출력합니다
 let    htmlstream = '';
 
