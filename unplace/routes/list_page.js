20caf7ec1a94320cb564db9e5523667fc2296a9f
leesing9 <63177746+leesing9@users.noreply.github.com>
2021-01-10 05:58:32 +0900

    v3.1

---
+++ unplace/routes/list_page.js
@@ -0,0 +1,36 @@
+// 메인화면 출력파일
+const  express = require('express');
+const  ejs = require('ejs');
+const  fs = require('fs');
+const  router = express.Router();
+var    loglevel = 1;
+
+const  GetMainUI = (req, res) => {   // 메인화면을 출력합니다
+let    htmlstream = '';
+
+     logging(loglevel, '  GetMainUI() 호출 ! ');
+     res.render('list_page');
+      
+      // { 'title' : '쇼핑몰site',
+      // 'logurl': '/users/logout',
+      // 'loglabel': '로그아웃',
+      // 'regurl': '/users/profile',
+      // 'reglabel':req.session.who,  // 세션에 저장된 사용자명표시
+      // 'buyurl':'/users/buyprod',
+      // 'buylabel' : '구매할래?'});
+
+
+};
+
+const logging = (level, logmsg) => {
+       if (level != 0) {
+         console.log(level, logmsg)
+         loglevel++;
+      }
+}
+
+// ‘/’ get 메소드의 핸들러를 정의
+router.get('/', GetMainUI);
+
+// 외부로 뺍니다.
+module.exports = router
---
+++ unplace/server.js
@@ -0,0 +1,43 @@
+// Node.JS 내외부 모듈추출
+const   express = require('express');
+const   cookieParser = require('cookie-parser');
+const   session = require('express-session');
+const   bodyParser = require('body-parser');
+const   createError = require('http-errors');
+const   path = require('path');
+const   app = express();
+
+const   mainui = require('./routes/mainui');
+const   firstpage = require('./routes/firstpage');
+const   login = require('./routes/login');
+const   list_page = require('./routes/list_page');
+//주소 설정
+const   PORT = 65007;
+
+// 실행환경 설정부분
+app.set('views', path.join(__dirname, 'views'));  // views경로 설정
+app.set('view engine', 'ejs');                    // view엔진 지정
+app.use(express.static(path.join(__dirname, 'public')));   // public설정
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({ key: 'sid',
+                  secret: 'secret key',  // 세션id 암호화할때 사용
+                  resave: false,         // 접속할때마다 id부여금지
+                  saveUninitialized: true })); // 세션id사용전에는 발급금지
+
+// URI와 핸들러를 매핑
+app.use('/', firstpage);       // URI (/) 접속하면 firstpage로 라우팅
+app.use('/mainui',mainui);
+app.use('/login',login);
+app.use('/list_page',list_page);
+// app.use('/log', login);
+// app.use('/log', login);
+// app.use('/log', login);
+
+
+// 서버를 실행합니다.
+app.listen(PORT, function () {
+       console.log('서버실행: http://localhost:' + PORT);
+});
